792f07e91a6bad23ded88b3b1d4140317fbec82b
Ricardo Ledan <ricoledan@gmail.com>
2023-08-13 19:04:01 -0400

    added cors to nodejs app

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./controllers');
 const exphbs = require("express-handlebars")
 const path = require("path")
@@ -14,6 +15,7 @@ const hbs = exphbs.create();
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
