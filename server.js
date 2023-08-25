aaee844b29a50884b49bad4e42cec480dfcccdd5
ThalVal <vthalissa@yahoo.com>
2023-08-25 01:55:06 -0700

    edits

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./controllers');
-const exphbs = require("express-handlebars")
 const path = require("path")
 const {User, Bird} = require('./models');
 const sequelize = require('./config/connection');
@@ -10,9 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create();
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
