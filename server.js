6adff72e5f1ad27ea6a9f19628cc8f181e8103b4
Ricardo <ricoledan@gmail.com>
2023-08-27 18:59:11 -0400

    updates

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create();
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
